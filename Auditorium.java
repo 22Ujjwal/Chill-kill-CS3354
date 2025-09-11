6e7738d5495b338aa5374d6c2a981a1c511af9e9
Sreeja A <156006746+OddEyed45@users.noreply.github.com>
2025-09-11 16:52:34 -0500

    Revert "Thuhuong branch"

--- Auditorium.java
+++ Auditorium.java
@@ -98,6 +98,7 @@ class Auditorium{
 
 		//---------------------------------------------------------------------------------------
 		System.out.print("   ");
+		
 
         // Display seat letter at the top and row position at the left
 		int cols = colCount();
